feea7b80b537cbe2632cea2cf89720957af5dc7d
Lenna <lenna@lennamau.com>
2019-03-04 14:58:47 -0700

    success function written

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,27 +1,50 @@
 module.exports = {
-    success,
-    fail,
-    repair
-}
+  success,
+  fail,
+  repair
+};
 
 function success(item) {
-    item.enhancement++
-    return item
+    if(item.enhancement<= 14 && item.durability > 25){
+        item.enhancement++;
+        item.name = `[${generateName(item.enhancement)}] ${item.name}`;
+        return item;
+    }
+  if(item.enhancement >= 15 && item.durability > 10) {
+    item.enhancement++;
+    item.name = `[${generateName(item.enhancement)}] ${item.name}`;
+    return item;
+  }
+  return 'Item durability too low'
 }
 
-function fail(item) {
-
-
-}
+function fail(item) {}
 
 function repair(item) {
-    return ({...item, durability: 100})
+  return { ...item, durability: 100 };
 }
 
 function generateName(level) {
-    if(level === 0) {
-        return ''
-    } else if(level> 0 && level < 16) {
-       return `+${level}`
+  if (level === 0) {
+    return "";
+  } else if (level > 0 && level < 16) {
+    return `+${level}`;
+  } else {
+    switch (level) {
+      case 16:
+        return "PRI";
+
+      case 17:
+        return "DUO";
+
+      case 18:
+        return "TRI";
+
+      case 19:
+        return "TET";
+
+      case 20:
+        return "PEN";
     }
+  }
 }
--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,12 +1,35 @@
-const { success, fail, repair } = require('./enhancer')
+const { success, fail, repair } = require("./enhancer");
 
-describe('repair', () => {
-    it('sets durability to 100', () =>{
-        const item = {name: 'sword', type: 'weapon', durability: 80, enhancement:5}
-        expect(repair(item)).toEqual({name: 'sword', type: 'weapon', durability: 100, enhancement: 5})
-    })
-})
+describe("repair", () => {
+  it("sets durability to 100", () => {
+    const item = {
+      name: "sword",
+      type: "weapon",
+      durability: 80,
+      enhancement: 5
+    };
+    expect(repair(item)).toEqual({
+      name: "sword",
+      type: "weapon",
+      durability: 100,
+      enhancement: 5
+    });
+  });
+});
 
-describe('success', () => {
-  
-})
+describe("success", () => {
+  it("increments enhancement and sets name", () => {
+    const item = {
+      name: "sword",
+      type: "weapon",
+      durability: 80,
+      enhancement: 5
+    };
+    expect(success(item)).toEqual({
+      name: "[+6] sword",
+      type: "weapon",
+      durability: 80,
+      enhancement: 6
+    });
+  });
+});
