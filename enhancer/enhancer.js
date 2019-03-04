4176ca45dc8efaf0d66cca45e7cd019e5a0f976a
Lenna <lenna@lennamau.com>
2019-03-04 16:05:29 -0700

    fail completed

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -5,20 +5,50 @@ module.exports = {
 };
 
 function success(item) {
-    if(item.enhancement<= 14 && item.durability > 25){
-        item.enhancement++;
-        item.name = `[${generateName(item.enhancement)}] ${item.name}`;
-        return item;
-    }
-  if(item.enhancement >= 15 && item.durability > 10) {
+  if (item.enhancement <= 14 && item.durability > 25) {
     item.enhancement++;
-    item.name = `[${generateName(item.enhancement)}] ${item.name}`;
+    item.displayName = `[${generateName(item.enhancement)}] ${item.name}`;
     return item;
   }
-  return 'Item durability too low'
+  if (item.enhancement >= 15 && item.durability > 10) {
+    item.enhancement++;
+    item.displayName = `[${generateName(item.enhancement)}] ${item.name}`;
+    return item;
+  }
+  return "Item durability too low";
 }
 
-function fail(item) {}
+function fail(item) {
+  if (item.type === "armor") {
+    if (item.enhancement < 5) {
+      return "You cannot fail, your enhancement too low";
+    } else {
+      if (item.enhancement >= 5 && item.enhancement <= 14) {
+        item.durability = item.durability - 5;
+        return item;
+      } else {
+        item.durability = item.durability - 10;
+        item.durability > 16 ? item.enhancement-- : item.enhancement;
+        item.displayName = `[${generateName(item.enhancement)}] ${item.name}`;
+        return item;
+      }
+    }
+  } else if (item.type === "weapon") {
+    if (item.enhancement < 7) {
+      return "You cannot fail, your enhancement too low";
+    } else {
+      if (item.enhancement >= 5 && item.enhancement <= 14) {
+        item.durability = item.durability - 5;
+        return item;
+      } else {
+        item.durability = item.durability - 10;
+        item.durability > 16 ? item.enhancement-- : item.enhancement;
+        item.displayName = `[${generateName(item.enhancement)}] ${item.name}`;
+        return item;
+      }
+    }
+  }
+}
 
 function repair(item) {
   return { ...item, durability: 100 };
